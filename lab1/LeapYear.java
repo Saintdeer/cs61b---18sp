451f828caca9a924fa9b86b367f78d9ca729941b
Saintdeer <theartofstain@gmail.com>
2023-08-06 03:27:31 +0800

    completed rest part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,15 @@ public class LeapYear {
         }
     }
 
+    /** To test whether or not a given year is a Leap Year.  */
+    public static boolean isLeapYear(int year){
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
