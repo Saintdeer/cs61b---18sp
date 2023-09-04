58a8c146fe3f6971a660f8ea11bc891894ed4e80
Saintdeer <theartofstain@gmail.com>
2023-09-04 21:49:37 +0800

    completed hw1

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,23 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    protected int fillCount;
+    protected int capacity;
+
+    @Override
+    public int capacity() {
+        return capacity;
+    }
+
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+
+    public abstract T peek();
+
+    public abstract T dequeue();
+
+    @Override
+    public abstract void enqueue(T x);
+}
