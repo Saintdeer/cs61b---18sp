8562a61699c3631e8d73b7f185f348e642c4eac6
Saintdeer <theartofstain@gmail.com>
2023-09-04 21:57:08 +0800

    fix some bugs with hw1

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -13,11 +13,4 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     public int fillCount() {
         return fillCount;
     }
-
-    public abstract T peek();
-
-    public abstract T dequeue();
-
-    @Override
-    public abstract void enqueue(T x);
 }
